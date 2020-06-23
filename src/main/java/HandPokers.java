c518c3e63f25ef023a3ceaeae6072597ffc8f2fa
shoor <shoorday@gmail.com>
2020-06-23 20:58:31 +0800

    first commit

---
+++ src/main/java/HandPokers.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class HandPokers {
+    private List<Poker> pokers;
+    private Boolean flush;
+    private Boolean straight;
+    private Boolean onePair;
+    private Boolean twoPair;
+    private Boolean threeOfAKind;
+
+    public Boolean vs(HandPokers opponent) {
+        return null;
+    }
+
+    public void load() {
+
+    }
+
+}
